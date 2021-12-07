0864b7f85f3688fa125df905d88a6942f39aeef0
Michael Sampaio <tradsampaio@gmail.com>
2021-12-07 18:24:47 -0300

    Revert "Rotas"
    This reverts commit 2935bdd7e4e67e59b7150a24ff1ac0f1559cba88.

--- Aula04/src/pages/Home/index.jsx
+++ Aula04/src/pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 import { styles } from './styles';
-import Card from '../../components/Card';
 
 export default function Home() {
     const [text, setText] = useState("");
@@ -16,7 +15,9 @@ export default function Home() {
     <View style={styles.container}>
       <TextInput style={styles.input} value={text} onChangeText={setText} />
       <Button title="Inserir Item" onPress={handleClick} />
-      <Card list={listItem} />
+      {listItem.map((item) => (
+          <Text>{item}</Text>
+      ))}
     </View>
   );
 }
