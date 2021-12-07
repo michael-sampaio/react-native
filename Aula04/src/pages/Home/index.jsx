2935bdd7e4e67e59b7150a24ff1ac0f1559cba88
Michael Sampaio <tradsampaio@gmail.com>
2021-12-07 18:23:48 -0300

    Rotas

--- Aula04/src/pages/Home/index.jsx
+++ Aula04/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 import { styles } from './styles';
+import Card from '../../components/Card';
 
 export default function Home() {
     const [text, setText] = useState("");
@@ -15,9 +16,7 @@ export default function Home() {
     <View style={styles.container}>
       <TextInput style={styles.input} value={text} onChangeText={setText} />
       <Button title="Inserir Item" onPress={handleClick} />
-      {listItem.map((item) => (
-          <Text>{item}</Text>
-      ))}
+      <Card list={listItem} />
     </View>
   );
 }
