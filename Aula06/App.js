906364a063d88e523160ec8646ed0ff5eb13d35e
Michael Sampaio <tradsampaio@gmail.com>
2021-12-08 22:52:29 -0300

    atualizacao

--- Aula06/App.js
+++ Aula06/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Image, View, StyleSheet } from 'react-native';
+import { Button, Image, View, StyleSheet, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 import gato from './assets/gato.jpg';
 
@@ -9,26 +9,34 @@ export default function App() {
 
   const handleClick = async () => {
 
-    setLoading(true)
+    setLoading(true);
 
     await axios({
       method: "GET",
       url: "https://api.thecatapi.com/v1/images/search",
       responseType: "json",
     }).then((response) => {
-      setApiData(response.data[0].url)});
-
-    setLoading(false);
-    console.log("TERMINEI A FUNÇÃO");
+      setApiData(response.data[0].url)
+    });
+    console.log("TERMINEI A FUNÇÃO")
   };
 
   return ( 
     <View style = {styles.container} >
       <View style = {styles.imgContainer} >
-        <Image style={styles.img} source = {gato} resizeMode = 'contain' / >
-      </View> 
+        <Image 
+          source={apiData ? { uri: apiData } : gato}
+          style={styles.img}  
+          resizeMode='contain' 
+          onLoadEnd={() => setLoading(false)} 
+        />
+      </View>
+        {loading && <ActivityIndicator size='large' color='white'/>}
       <View style = {styles.btnContainer} >
-        <Button title = "VEJA OUTRO GATINHO" onPress = {handleClick} disabled = {loading}/>
+        <Button 
+          title = "VEJA OUTRO GATINHO" 
+          onPress = {handleClick} 
+          disabled = {loading}/>
       </View> 
     </View>
   );
